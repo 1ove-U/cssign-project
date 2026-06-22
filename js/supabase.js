4e848ae637129d613e3fb2dcfc1a1ce7e5b9f25a
1ove-U <zillergotspw@gmail.com>
2026-06-22 10:17:30 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -7,7 +7,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJibHFycHdydnRvdGF6dG9icGZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwNzY5MzIsImV4cCI6MjA5NzY1MjkzMn0.qAmRDZZqOfDD8GpSBIimtjKaPbMIcU2LbA45rnUPAEs';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
