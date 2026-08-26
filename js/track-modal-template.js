e93f8dce64f6c923c784fa4525bcb845e210887b
1ove-U <zillergotspw@gmail.com>
2026-08-26 09:36:37 +0700

    Update track-modal-template.js

--- js/track-modal-template.js
+++ js/track-modal-template.js
@@ -51,6 +51,12 @@
         </button>
       </form>
 
+      <div class="tm-lookup-pill" id="tm-lookup-pill" style="display:none;">
+        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.13.96.36 1.9.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.9.34 1.85.57 2.81.7A2 2 0 0 1 22 16.92Z"/></svg>
+        <span>ยืนยันด้วยเบอร์ลงท้าย <b id="tm-lookup-phone"></b></span>
+        <button type="button" class="tm-lookup-again" id="tm-lookup-again-btn">เช็คใบอื่น</button>
+      </div>
+
       <div class="tm-result" id="tm-result"></div>
 
       <p class="tm-help">
